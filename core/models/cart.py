967d5eaf645a011490f70623ee9af9aa1b24ddc2
henriquekops <henriquekops@gmail.com>
2019-11-11 23:08:02 -0300

    add cart model (singleton)

--- core/models/cart.py
+++ core/models/cart.py
@@ -1,3 +1,26 @@
 
+
 class CartModel:
-    pass
+
+    class __CartModelInstance:
+
+        def __init__(self):
+            self.__storage = list()
+
+        def add(self, product):
+            return self.__storage.append(product)
+
+        def rem(self, product_id):
+            return self.__storage.pop(product_id)
+
+        def show(self):
+            return self.__storage
+
+    instance = None
+
+    def __init__(self):
+        if not CartModel.instance:
+            CartModel.instance = CartModel.__CartModelInstance()
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
