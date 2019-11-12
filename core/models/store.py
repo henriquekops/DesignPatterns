bc8230ecdd1643a9569d1b361b344e3efd49580c
carlo laitano <carlo.laitano@acad.pucrs.br>
2019-11-12 01:03:55 -0300

    to build StoreModel class (Singleton)

--- core/models/store.py
+++ core/models/store.py
@@ -1,3 +1,51 @@
+""" Store model class """
 
-class CartModel:
-    pass
+
+class StoreModel:
+
+    __instance = None
+
+    class __StoreModelInstance:
+
+        def __init__(self):
+            self.__products = list()
+
+        def add_product(self, product):
+
+            """ Method to add procucts into sale list """
+
+            try:
+
+                self.__products.append(product)
+
+            except Exception as e:
+                raise Exception("Error to add product {}".format(str(e)))
+
+        def remove_product(self, product_index):
+
+            """ Method to remove procucts into sale list """
+
+            try:
+
+                self.__products.pop(product_index)
+
+            except Exception as e:
+                raise Exception('Error to remove product {}'.format(str(e)))
+
+        def show_products(self):
+
+            """ Method to show all products in store """
+
+            return self.__products
+
+    def __init__(self):
+        if not self.__instance:
+            StoreModel.__instance = StoreModel.__StoreModelInstance()
+        print(self.__instance)
+
+    def __getattr__(self, name):
+        return getattr(self.__instance, name)
+
+
+if __name__ == "__main__":
+    import pdb; pdb.set_trace()
