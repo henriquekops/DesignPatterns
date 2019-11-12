f74c2075e12a2c5d32603218c56ea00f2a0b7d21
henriquekops <henriquekops@gmail.com>
2019-11-12 00:16:21 -0300

    upgrade product (factory logic)

--- core/models/product.py
+++ core/models/product.py
@@ -1,47 +1,49 @@
 
 # built-in dependencies
 from random import randint
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
-class ProductModel(ABC):
-
-    @abstractmethod
-    def factory_method(self):
-        pass
+class __Product(ABC):
 
     def __str__(self):
         return ', '.join([f'{k}:{v}' for k, v in self.__dict__.items()])
 
 
-class HealthProduct(ProductModel):
+class HealthProduct(__Product):
 
     def __init__(self):
         self.name = "Health Product"
         self.price = randint(1, 20)
         self.type = "Health"
 
-    def factory_method(self):
-        return HealthProduct()
 
-
-class TechProduct(ProductModel):
+class TechProduct(__Product):
 
     def __init__(self):
         self.name = "Tech Product"
         self.price = randint(1, 20)
         self.type = "Tech"
 
-    def factory_method(self):
-        return TechProduct()
 
-
-class HomeProduct(ProductModel):
+class HomeProduct(__Product):
 
     def __init__(self):
         self.name = "Home Product"
         self.price = randint(1, 20)
         self.type = "Home"
 
-    def factory_method(self):
-        return HomeProduct()
+
+class ProductModel:
+
+    __classes = dict(
+        health=HealthProduct,
+        tech=TechProduct,
+        home=HomeProduct
+    )
+
+    @classmethod
+    def factory_method(cls, method):
+        def __error():
+            raise NotImplementedError
+        return cls.__classes.get(method, __error)()
